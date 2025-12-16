e33943afc3a00f80d3807205df19dd675dd71db7
Zainab Raza <zainabraza1960@gmail.com>
2025-12-16 14:52:28 +0500

    cookies set

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -225,7 +225,7 @@ export async function googleCallback(req, res) {
   res.cookie("token", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
+    sameSite: "none",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 
