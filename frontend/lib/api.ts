7edeeb5e3f8e83fc7b7f5970aa5b1097f4b4746b
Zainab Raza <zainabraza1960@gmail.com>
2025-12-16 11:09:52 +0500

    updated backend URL

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -2,7 +2,7 @@
 import axios, { AxiosInstance } from "axios";
 
 export const api: AxiosInstance = axios.create({
-  baseURL: "http://localhost:5001/api", // your backend URL
+  baseURL: "https://reon-4g0b.onrender.com/api", // your backend URL
   withCredentials: true,
   timeout:30000,
 });
